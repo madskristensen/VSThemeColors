7b514570b373845efc2d713f22b0efc58fe455e4
Mads Kristensen <madskristensen@users.noreply.github.com>
2021-06-15 15:02:02 -0700

    Split extension to add VS 2022 support

--- src/VSThemeColors2022/source.extension.cs
+++ src/VSThemeColors2022/source.extension.cs
@@ -7,8 +7,8 @@ namespace VSThemeColors
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "6ad27930-f7a1-4888-a172-c6fa4440e7a0";
-        public const string Name = "VS Theme Colors";
+        public const string Id = "6ad27930-f7a1-4888-a172-c6fa4440e7a1";
+        public const string Name = "VS Theme Colors 2022";
         public const string Description = @"Makes it easy to browse all the brushes and select the right theme color for your Visual Studio extension UI.";
         public const string Language = "en-US";
         public const string Version = "1.0";
